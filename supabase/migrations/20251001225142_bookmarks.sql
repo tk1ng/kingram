bc124e856f0f8cee27ef146d0986651a9cdbc8c5
Taryn King <marshaeking@gmail.com>
2025-10-01 17:54:14 -0500

    add bookmarks table schema

---
+++ supabase/migrations/20251001225142_bookmarks.sql
@@ -0,0 +1,8 @@
+create table public.bookmarks (
+    id integer primary key generated always as identity,
+    name text
+    url text
+);
+
+alter table public.bookmarks
+enable row level security;
